3602d41e106f9b5f4bd929c258783b30cb7b2683
MP <themp731@gmail.com>
2019-08-29 15:55:33 -0400

    About halfway through

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,21 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/super_simple')
+def super_simple():
+    return jsonify(message='This is Super Simple...or is it?')
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That wasn\'t found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = ''
+    age = 0
 
 
 if __name__ == '__main__':
